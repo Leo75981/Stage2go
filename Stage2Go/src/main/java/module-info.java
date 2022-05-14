fc7ec502dc6277f237cabe2c60492912df4cfb4b
Leo75981 <95914524+Leo75981@users.noreply.github.com>
2022-05-14 14:14:35 +0200

    Delete Stage2Go directory

--- Stage2Go/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.staggers {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.staggers to javafx.fxml;
-    exports com.example.staggers;
-}
